f2b20f17f135f18d4b43dcbe74d801b064f6e662
e-morales <e.morales@digitalnest.org>
2019-02-24 19:13:16 -0800

    roboFreinds setup

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return ( 
+        <div className='f1 tc'>
+            <h1>Hello World</h1>
+            <h2> I'm learning React!</h2>
+        </div>)
+    }
+}
+
+export default Hello;
